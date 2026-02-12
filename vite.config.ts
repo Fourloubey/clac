ac78c505bb8f5d8ee11e9e374b81e4e6a1786aad
PacoAugustin <gaetan.fourloubey@gmail.com>
2026-02-12 21:52:04 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/clac/" : "/",
+  base:"/clac/",
   server: {
     host: "::",
     port: 8080,
