d165973cca97e0fc5622655e02c96c13ccca89b9
PacoAugustin <gaetan.fourloubey@gmail.com>
2026-02-20 23:07:02 +0100

    Update Ariette.tsx

--- src/pages/projets/Ariette.tsx
+++ src/pages/projets/Ariette.tsx
@@ -19,21 +19,21 @@ import ariette15 from "@/assets/ariette/ariette-15.jpg";
 
 // 2. Organisation du tableau des images
 const images = [
-  { src: ariette1, alt: "Projet Ariette – Photo 1" },
+  { src: ariette1, alt: "Projet Ariette – Photo 4" },
   { src: ariette2, alt: "Projet Ariette – Photo 2" },
   { src: ariette3, alt: "Projet Ariette – Photo 3" },
-  { src: ariette4, alt: "Projet Ariette – Photo 4" },
+  { src: ariette4, alt: "Projet Ariette – Photo 1" },
   { src: ariette5, alt: "Projet Ariette – Photo 5" },
   { src: ariette6, alt: "Projet Ariette – Photo 6" },
   { src: ariette7, alt: "Projet Ariette – Photo 7" },
   { src: ariette8, alt: "Projet Ariette – Photo 8" },
   { src: ariette9, alt: "Projet Ariette – Photo 9" },
   { src: ariette10, alt: "Projet Ariette – Photo 10" },
-  { src: ariette11, alt: "Projet Ariette – Photo 11" },
+  { src: ariette11, alt: "Projet Ariette – Photo 15" },
   { src: ariette12, alt: "Projet Ariette – Photo 12" },
   { src: ariette13, alt: "Projet Ariette – Photo 13" },
   { src: ariette14, alt: "Projet Ariette – Photo 14" },
-  { src: ariette15, alt: "Projet Ariette – Photo 15" },
+  { src: ariette15, alt: "Projet Ariette – Photo 11" },
 ];
 
 // 3. Texte avec tes mots-clés en gras
